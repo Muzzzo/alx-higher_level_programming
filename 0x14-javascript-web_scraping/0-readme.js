1a8759569f1cffe5a5f996765a3210bd8d5289e3
Muzzzo <musondachristopher@rocketmail.com>
2024-02-21 14:41:48 -0800

    0-readme.js

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,34 +1,12 @@
-#!/usr/bin/env node
-const fs = require('fs');
+#!/usr/bin/node
+// Read from file
 
-// Check if the file path argument is provided
-if (process.argv.length < 3) {
-  console.error('Usage: node read_file.js <file_path>');
-  process.exit(1);
-}
-
-// Extract the file path from the command-line arguments
-const filePath = process.argv[2];
-
-// Check if the file exists
-if (!fs.existsSync(filePath)) {
-  console.error(`File '${filePath}' does not exist.`);
-  process.exit(1);
-}
-
-// Check if the file is readable
-fs.access(filePath, fs.constants.R_OK, (err) => {
-  if (err) {
-    console.error(`File '${filePath}' is not accessible: ${err}`);
-    process.exit(1);
-  }
-
-  // Read the content of the file
-  fs.readFile(filePath, 'utf8', (err, data) => {
+const filesys = require('fs');
+filesys.readFile(process.argv[2], 'utf-8',
+  function (err, data) {
     if (err) {
-      console.error(err);
+      console.log(err);
       return;
     }
     console.log(data);
   });
-});
